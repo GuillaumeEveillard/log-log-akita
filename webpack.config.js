efaa09d8e11932434e171b9616d3f8e95a53e215
geveillard <geveillard@murex.com>
2020-10-05 14:50:51 +0200

    Cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,8 @@ module.exports = (env, argv) => {
     entry: './bootstrap.js',
     output: {
       path: distPath,
-      filename: "todomvc.js",
-      webassemblyModuleFilename: "todomvc.wasm"
+      filename: "loglogakita.js",
+      webassemblyModuleFilename: "loglogakita.wasm"
     },
     plugins: [
       new CopyWebpackPlugin([
